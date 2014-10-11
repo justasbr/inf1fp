d5eb4d3addbc6cb0276e4ee11eda7d5dc4f8f799
Justas Brazauskas <justasbr@gmail.com>
2014-10-11 09:59:58 +0100

    tut2 challenges

--- tutorial2.hs
+++ tutorial2.hs
@@ -28,7 +28,8 @@ makeKey n = zip ['A'..'Z'] (rotate n ['A'..'Z'])
 
 -- 4.
 lookUp :: Char -> [(Char, Char)] -> Char
-lookUp c xs = head ([b | (a,b) <- xs, a == c] ++ [c])
+lookUp c xs = if not (null list) then head list else c
+		where list = [b | (a,b) <- xs, a == c]
 
 lookUpRec :: Char -> [(Char,Char)] -> Char
 lookUpRec c [] = c
@@ -82,26 +83,51 @@ candidates x = [ (n, decipherStr n x) | n <- [0..25],
 
 -- 12.
 splitEachFive :: String -> [String]
-splitEachFive = undefined
+splitEachFive [] = []
+splitEachFive x | length x > 4 = (take 5 x) : (splitEachFive (drop 5 x))
+				| otherwise = (take n x ++ replicate (5-n) 'X') : splitEachFive []
+				where n = length x
+splitEach :: Int -> String -> [String]
+splitEach n [] = []
+splitEach n x = (take n x) : (splitEach n (drop n x))
 
 -- 13.
 prop_transpose :: String -> Bool
-prop_transpose = undefined
+prop_transpose x = (transpose (transpose (splitEachFive x)) == splitEachFive x)
 
 -- 14.
+makeStr :: [[Char]] -> String
+makeStr [] = []
+makeStr (x:xs) = x ++ makeStr xs
+
 encrypt :: Int -> String -> String
-encrypt = undefined
+encrypt n x = makeStr (transpose (splitEachFive (encipherStr n x)))
 
 -- 15.
 decrypt :: Int -> String -> String
-decrypt = undefined
+decrypt n x = decipherStr n (makeStr (transpose (splitEach fifth x)))
+			where fifth = div (length x) 5
 
+prop_encrypt :: Int -> String -> Bool
+prop_encrypt n x = ((normalize x) `isPrefixOf` (decrypt nSmall (encrypt nSmall x)))
+			where nSmall = mod n 25
 -- Challenge (Optional)
 
 -- 16.
 countFreqs :: String -> [(Char, Int)]
-countFreqs = undefined
+countFreqs str = [(x,c) | x<-['A'..'z'], let c = (length.filter (==x)) str, c>0]
+
+lookUpInt :: Char -> [(Char, Int)] -> Int
+lookUpInt c xs = if not (null list) then head list else 0
+		where list = [b | (a,b) <- xs, a == c]
 
 -- 17
 freqDecipher :: String -> [String]
-freqDecipher = undefined
+freqDecipher x = sortByFreq ([ decrypt n x | n <- [0..25]])
+
+sortByFreq :: [String] -> [String]
+sortByFreq [] = []
+sortByFreq (x:xs) = sortByFreq([a | a <- xs, (freq 'E' a) > (freq 'E' x)]) ++ 
+			  [x] ++ 
+		      sortByFreq([b | b <- xs, (freq 'E' b) <= (freq 'E' x)])
+			  where freq c str = lookUpInt c (countFreqs str)
